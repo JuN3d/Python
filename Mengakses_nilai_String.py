32cefae90360a51b8a3340d6106da0f3191b3764
JuN3d <77531451+JuN3d@users.noreply.github.com>
2021-01-18 23:58:14 +0700

    Add files via upload

---
+++ Mengakses_nilai_String.py
@@ -0,0 +1,4 @@
+name = 'Maulana Ali'
+message = "maulana Ali belajar bahasa python di Belajarpython"
+print ("name[0]: ", name[0])
+print ("message[1:4]: ", message[1:4])
---
+++ Mengupdate_String.py
@@ -0,0 +1,2 @@
+message = 'Hello world!'
+print("Update String :- ", message[:6] + 'Python')
